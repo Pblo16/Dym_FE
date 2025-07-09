dc8925a2b4ba0e880ec5985bb12d7d99593f7f79
Pablo Hernandez Villegas <38545507+Pblo16@users.noreply.github.com>
2025-07-09 11:04:57 -0600

    Actualizar Products.tsx

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,21 +1,9 @@
 import type Product from "@/interfaces/product";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import { Filter } from "@/components/Filter";
 import { ProductsGrid } from "@/components/ProductsGrid";
 
-
-interface ProductsApiResponse {
-    data: Product[];
-    meta: {
-        pagination: {
-            page: number;
-            pageSize: number;
-            pageCount: number;
-            total: number;
-        };
-    };
-}
 
 function Products() {
     const [sortOrder, setSortOrder] = useState<string>("createdAt:desc");
