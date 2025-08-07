2f3fb4a1e3529a5ae37f96f25c7d60077fded42f
Pblo16 <pablo.hv.2002@gmail.com>
2025-08-07 16:01:47 -0600

    fix: Add minimum height to Products component for better layout consistency

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -8,7 +8,7 @@ function Products() {
     const [limit] = useState<number | null>(null);
 
     return (
-        <div className="flex flex-col gap-8 md:grid md:grid-cols-[1fr_4fr] py-4">
+        <div className="flex flex-col gap-8 md:grid md:grid-cols-[1fr_4fr] py-4 min-h-[calc(100vh-11rem)]">
             <Filter sortOrder={sortOrder} setSortOrder={setSortOrder} title="Filtrar Productos" />
             <ProductsGrid sortOrder={sortOrder} limit={limit} />
         </div>
